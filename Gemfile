d184f12b3a07364e7e0506e421d576cb9850da78
Michael Caldwell <bigmic69@gmail.com>
2017-06-12 12:06:54 -0500

    Bg-img Update
    Fine tuning the ol' numbers...

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@
 source "https://rubygems.org"
 
 # gem "rails"
+gem 'sass'
+gem 'sass-rails'
+gem 'chunky_png', '~> 1.3', '>= 1.3.8'
--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     $('.header').click(function(){
-        $('h1').hide();
+        $('h1').toggle();
     });
 });
